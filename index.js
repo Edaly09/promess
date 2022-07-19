c219b3da193e54d00390d9059aed48afda6a3c84
Edaly09 <edalyminthe@gmail.com>
2022-07-19 14:16:44 +0200

    promiss

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
-const myCondition = () =>{
+// const myCondition = () =>{
+
+//     return new Promise ((resolve, rejet) =>{
+//         const holySpritGhost = 10
+
+//         setTimeout(()=>{
+//             if (holySpritGhost >= 100){
+//                 resolve("Hello")
+//             }
+//             else{
+//                 rejet("je ne salut personne ")
+//             }
+
+//         }, 2000)
+       
+//     })
+// }
+
+// const myConditionGeneral = async ()=>{
+
+//     try {
+//         console.log(" j'execute mon code")
+//         console.log("j'attends 2 secondes")
+//         const answer = await myCondition()
+//         console.log(answer)
+//     } catch (error) {
+//         console.log("failed")
+//         console.log(error)
+//     }
+// }
+//  myConditionGeneral();
+
+// ***************TEMPERATURE *******************************
+
+ const temeratureOfday = () =>{
 
     return new Promise ((resolve, rejet) =>{
-        const holySpritGhost = 100
+        const temperature = 11
 
         setTimeout(()=>{
-            if (holySpritGhost >= 100){
-                resolve("Hello")
+            if (temperature <= 10){
+                resolve("Il fait froid")
             }
             else{
-                rejet("je salut personne ")
+                rejet("il fait chaud ")
             }
 
-        })
+        }, 4000)
        
     })
 }
 
-const myConditionGeneral = async ()=>{
+const temps = async ()=>{
 
     try {
         console.log(" j'execute mon code")
-        console.log("j'attends 2 secondes")
-        const answer = await myCondition()
-        console.log(answer)
+        console.log("j'attends 4 secondes")
+        const resultat = await temeratureOfday()
+        console.log(resultat)
     } catch (error) {
-        console.log("failed")
+        console.log("blabla")
         console.log(error)
     }
 }
- myConditionGeneral();
+temps();
